363988af8d5e1cae08333bd1c36c706c42c13c16
romanenko <lifeapricot@gmail.com>
2022-05-30 12:18:05 +0300

    new lesson

--- lesson_1.py
+++ lesson_1.py
@@ -1,5 +1,34 @@
+# Урок 1 - Переменные
 
-a = 5
-b = 10
-c = a + b
-print('было', f"{b}", 'стало', c)
+number = 3
+number = 4 # Теперь переменная number ссылается на 4
+number2 = 5
+result = number + number2
+print(result)
+
+num1 = num2 = 5
+print(num1, num2)
+
+# Множиственное присвоение
+num_1, num_2 = 5, 7
+print(num_1, num_2)
+
+swap1 = 8
+swap2 = 9
+swap1, swap2 = swap2, swap1 + swap2
+print(swap1, swap2)
+
+swap2 -= number
+print(swap2)
+
+swap2 += 1
+print(swap2)
+
+print()
+
+# Распаковка списка по периметру
+
+*z, x, c = [1, 2, 3, 4 ,5]
+print(z)
+print(x)
+print(c)
