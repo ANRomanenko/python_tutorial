583dbba5e5006fd06bd636d4e03ab42734f663e8
andredance <lifeapricot@gmail.com>
2022-06-04 16:00:07 +0300

    new files

---
+++ lesson_3.3.py
@@ -0,0 +1,14 @@
+import os
+import time
+
+os.system('start https://www.instagram.com/')
+
+time.sleep(5)
+
+os.startfile("C://Program Files/Uninstall Tool/UninstallTool.exe")
+
+os.startfile()
+
+time.sleep(5)
+
+os.startfile()
